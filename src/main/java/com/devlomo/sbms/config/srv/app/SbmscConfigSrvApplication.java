c98c958c26585026da8ec1c1415c6b6cb80bdbf4
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-05-05 21:39:08 -0500

    :tada: Init
    begin of the project

---
+++ src/main/java/com/devlomo/sbms/config/srv/app/SbmscConfigSrvApplication.java
@@ -0,0 +1,13 @@
+package com.devlomo.sbms.config.srv.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmscConfigSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmscConfigSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlomo/sbms/config/srv/app/SbmscConfigSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlomo.sbms.config.srv.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmscConfigSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
